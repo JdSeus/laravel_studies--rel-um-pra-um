ba87b62901c06b3ea317ce2ce07d3b3b2550c239
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-19 11:03:05 -0300

    listando clientes e enderecos

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,45 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+use App\Models\Cliente;
+use App\Models\Endereco;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach($clientes as $c) {
+        echo "<p>ID: " . $c->id . "</p>";
+        echo "<p>Nome: " . $c->nome . "</p>";
+        echo "<p>Telefone: " . $c->telefone . "</p>";
+        $e = Endereco::where('cliente_id', $c->id)->first();
+        echo "<p>ID Cliente: " . $e->cliente_id . "</p>";
+        echo "<p>Rua : " . $e->rua . "</p>";
+        echo "<p>Numero : " . $e->numero . "</p>";
+        echo "<p>Bairro : " . $e->bairro . "</p>";
+        echo "<p>Cidade : " . $e->cidade . "</p>";
+        echo "<p>Uf : " . $e->uf . "</p>";
+        echo "<p>Cep : " . $e->cep . "</p>";
+        echo "<br>";
+    }
+});
+
+Route::get('/enderecos', function () {
+    $ends = Endereco::all();
+    foreach($ends as $e) {
+        echo "<p>ID Cliente: " . $e->cliente_id . "</p>";
+        echo "<p>Rua : " . $e->rua . "</p>";
+        echo "<p>Numero : " . $e->numero . "</p>";
+        echo "<p>Bairro : " . $e->bairro . "</p>";
+        echo "<p>Cidade : " . $e->cidade . "</p>";
+        echo "<p>Uf : " . $e->uf . "</p>";
+        echo "<p>Cep : " . $e->cep . "</p>";
+
+    }
+});
+
+
+
+
