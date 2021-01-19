980f07280293832e8fc3e7d809dbbe22682d30d9
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-19 11:14:46 -0300

    utilizando hasone

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+
+    public function endereco() {
+        return $this->hasOne('App\Models\Endereco');
+    }
 }
